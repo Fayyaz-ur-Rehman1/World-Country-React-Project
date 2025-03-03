f03b970bccaf4f99a6017fba3d96d9c18d488c52
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2025-03-03 18:23:12 +0530

    Add-Some-api-feature-in-country-page

--- world_country/src/Pages/Country.jsx
+++ world_country/src/Pages/Country.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import { Loader } from "../Components/UI/Loader";
+import { CountryCard } from "../Components/Layout/CountryCard";
+
 export function Country() {
+    const [isPanding, setTransition] = useTransition();
+    const [country, setCountry] = useState([]);
+
+    useEffect(() => {
+        setTransition(async () => {
+            const res = await getCountryData();
+            setCountry(res.data);
+        });
+    }, []);
+
+    if (isPanding) return <Loader />
+
     return (
-        <div>
-            <h1>Country Page</h1>
-        </div>
+        <section className="country-section">
+            <ul className="grid grid-four-cols">
+                {
+                    country.map((currCountry, index) => {
+                        return <CountryCard country={currCountry} key={index} />
+                    })
+                }
+            </ul>
+        </section>
     )
 }
